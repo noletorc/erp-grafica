b3f1a3efb0afcbe10e4f2028d5531d900b67017d
noletorc <162538208+noletorc@users.noreply.github.com>
2026-03-14 19:29:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,24 @@
 
+function abrirOrcamento(){
+document.getElementById('modalOrcamento').style.display='flex'
+}
+
+function fecharOrcamento(){
+document.getElementById('modalOrcamento').style.display='none'
+}
+
 function calcular(){
 
-let largura = document.getElementById('largura').value;
-let altura = document.getElementById('altura').value;
-let preco = document.getElementById('preco').value;
+let largura=document.getElementById('largura').value
+let altura=document.getElementById('altura').value
+let preco=document.getElementById('preco').value
+let qtd=document.getElementById('qtd').value
 
-let m2 = (largura/100)*(altura/100);
-let total = m2*preco;
+let m2=(largura/100)*(altura/100)
+let total=m2*preco*qtd
 
-document.getElementById('resultado').innerHTML =
-"Área: "+m2.toFixed(2)+" m² | Valor: R$ "+total.toFixed(2);
-
-}
-
-window.onload = function(){
-
-let g = document.getElementById('grafico');
-
-if(g){
-
-new Chart(g,{
-type:'bar',
-data:{
-labels:['Jan','Fev','Mar','Abr'],
-datasets:[{
-label:'Entradas',
-data:[3000,2800,3200,2900]
-},
-{
-label:'Saídas',
-data:[2000,2400,2500,2300]
-}]
-}
-})
-
-}
+document.getElementById('m2').value=m2.toFixed(2)
+document.getElementById('total').value=total.toFixed(2)
+document.getElementById('valorTotal').innerText="R$ "+total.toFixed(2)
 
 }
